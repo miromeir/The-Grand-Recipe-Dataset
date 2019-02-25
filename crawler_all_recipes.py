d282261dfad95351025ceac63c2e2d0c7314f68c
miromeir <miromeir@post.bgu.ac.il>
2019-02-25 19:37:01 +0200

    Crawler for allrecipes changes

--- crawler_all_recipes.py
+++ crawler_all_recipes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,22 +9,53 @@ import queue
 from threading import Thread
 
 class Crawler():
-    def crawl_for_recipes(self):
-        page_idx = 1
-        while(page_idx <= 57):
-            self.crawl_page_for_recipes("https://www.ihearteating.com/recipe-index-2/?fwp_paged="+str(page_idx))
-            page_idx = page_idx+1
-    def crawl_page_for_recipes(self, url):
-        print("crawling_page_for_recepies")
-        self.driver1.get(url)
-        mydivs = self.driver1.find_element_by_class_name("site-inner").find_elements_by_tag_name("article")
+    def crawl_for_grid_pages(self):
+        self.driver1.get("https://www.allrecipes.com/recipes/86/world-cuisine/?internalSource=top%20hubs&referringContentType=Homepage")
+        slider = self.driver1.find_element_by_id("insideScroll")
+        all_cuisins = slider.find_elements_by_tag_name("li")
+        
+        #get all types of cuisins from main page:
+        for li in all_cuisins:
+            link = li.find_element_by_tag_name("a").get_attribute("href")
+            cuisine_name = li.find_element_by_tag_name("span").text.replace("Recipes","").strip() #Remove the word 'Recipes'
+            self.grid_pages.put((cuisine_name,link))
+            
+    def crawl_grids(self):
+        while True:
+            if not self.grid_pages.empty():
+                grid_page_url = self.grid_pages.get()
+                self.crawl_grid_for_recipes(self, grid_page_url)
+                
 
-        for elem in mydivs:
+
+    def crawl_grid_for_recipes(self, url):
+        print("crawling_grid_for_recepies")
+        self.driver1.get(url)
+
+        grid = self.driver1.find_element_by_id("fixedGridSection")
+        cards = grid.find_elements_by_class_name("fixed-recipe-card")
+        
+        
+        
+        while True:
+            old_len = len(cards)
+            #Scroll to bottom
+            self.driver1.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            #Wait for grid to grow
+            time.sleep(5)
+            grid = self.driver1.find_element_by_id("fixedGridSection")
+            cards = grid.find_elements_by_class_name("fixed-recipe-card")
+            if old_len == len(cards):
+                break
+        
+        for card in cards:
             try:
-                link_tag = elem.find_element_by_tag_name("a")
+                img = card.find_elements_by_class_name("grid-card-image-container")
                 link_href = link_tag.get_attribute("href")
+                
                 self.to_visit_list.put(link_href)
             except Exception as e:
+                print(e)
                 pass
 
     def index_recipes(self):
@@ -69,8 +101,9 @@ class Crawler():
     def __init__(self):
 
         self.to_visit_list = queue.Queue()
+        self.grid_pages = queue.Queue()
         self.outputFile = open("output.txt", "a")
-
+        
         options = Options()
         options.headless = True
 
@@ -83,10 +116,10 @@ class Crawler():
         # firefox_options.add_argument("--proxy-bypass-list=*")
         #caps = DesiredCapabilities().CHROME
         #caps["pageLoadStrategy"] = "eager"  #  complete
-        self.driver1 = webdriver.PhantomJS(executable_path=os.path.abspath('/home/miro/Crawler/phantomjs'))
-        self.driver2 = webdriver.PhantomJS(executable_path=os.path.abspath('/home/miro/Crawler/phantomjs'))
-        index_thread = Thread(target = self.crawl_for_recipes)
-        index_thread.start()
+        self.driver1 = webdriver.PhantomJS(executable_path=os.path.abspath('./phantomjs'))
+        self.driver2 = webdriver.PhantomJS(executable_path=os.path.abspath('./phantomjs'))
+        find_grids_thread = Thread(target = self.crawl_for_grid_pages)
+        find_grids_thread.start()
 
         process_recipe_thread = Thread(target = self.index_recipes)
         process_recipe_thread.start()
